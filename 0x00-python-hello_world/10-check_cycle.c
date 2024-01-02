ea967a815c0a010da9bbed3de0d6e5d59281d6ca
Mohammed <6bouass9@gmail.com>
2024-01-02 12:27:29 -0800

    Mandatory tasks

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -1,17 +1,24 @@
 #include"lists.h"
 
+/**
+ * check_cycle - check if list is cycable
+ * @list: pointer to check list
+ *
+ * Return: 1 if cycable, 0 if not
+*/
+
 int check_cycle(listint_t *list)
 {
-	listint_t *prev = list, *nex = list;
+	listint_t *x = list, *y = list;
 
 	if (!list)
 		return (0);
-	while (nex && nex->next)
+	while (y && y->next)
 	{
-		prev = perv->next;
-		nex = nex->next->next;
-		if (prev = nex)
-			return (1);
+		x = x->next;
+		y = y->next->next;
+		if (x == y)
+		return (1);
 	}
 	return (0);
 }
