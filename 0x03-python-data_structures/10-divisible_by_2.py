39f8cbd6f015edfd283bb223d579532663206857
Mohammed <6bouass9@gmail.com>
2024-01-08 12:21:08 -0800

    Mandatory tasks

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,5 +1,12 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
+    if my_list is None:
+        return None
+    list_div = []
     for i in my_list:
-        return True if my_list[i] % 2 == 0 else False
+        if (i % 2) == 0:
+            list_div.append(True)
+        else:
+            list_div.append(False)
+    return list_div
         
