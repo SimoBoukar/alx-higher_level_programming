dc03c5263962571d91e09e3c0a9970391ad20912
Mohammed <6bouass9@gmail.com>
2024-01-08 12:07:45 -0800

    Mandatory tasks

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -3,5 +3,5 @@ def max_integer(my_list=[]):
     if len(my_list) < 1:
         return None
     list_cpy = my_list.copy()
-    sorted_list = my_list.sort()
-    return sorted_list(-1)
+    list_cpy.sort()
+    return list_cpy[-1]
