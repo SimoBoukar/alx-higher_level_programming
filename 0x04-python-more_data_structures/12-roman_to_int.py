77691361fd2702cc717b52ee05b11ce314f3065e
Mohammed <6bouass9@gmail.com>
2024-01-10 15:08:38 -0800

    Mandatory tasks

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,16 +1,14 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    if roman_string is None or not isinstance(roan_string, str):
+    if roman_string is None or not isinstance(roman_string, str):
         return 0
     tot = 0
-    cts_roman = 'I'
-    roman_num = {
-            'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000
-            }
-    for i in reversed(range(len(roman_string))):
-        if roman_num[roman_string[i]] >= roman[cts_roman]:
-            cts_roman = roman_string[i]
-            tot += roman[roman_string[i]]
+    cte_roman = 'I'
+    roman = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    for x in reversed(range(len(roman_string))):
+        if roman[roman_string[x]] >= roman[cte_roman]:
+            cte_roman = roman_string[x]
+            tot += roman[roman_string[x]]
         else:
-            tot -= roman[roman_string[i]]
-        return tot
+            tot -= roman[roman_string[x]]
+    return tot
