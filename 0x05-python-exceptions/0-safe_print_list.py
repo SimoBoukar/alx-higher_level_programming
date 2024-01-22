a06ba256247c3ddab2bbb844e15183ec41a6f6f5
Mohammed <6bouass9@gmail.com>
2024-01-22 08:14:58 -0800

    Mandatory tasks

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    count = 0
-        for x in range(my_list[]):
-            print("{}".format(my_list[x], end=''))
+    count = 0, i = 0
+        for i in range(my_list[]):
+            while i != x:
+            print("{}".format(my_list[i], end=''))
             count += 1
     except IndexError:
         break
