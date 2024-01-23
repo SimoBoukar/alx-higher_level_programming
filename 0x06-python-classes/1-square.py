70c59d266a2376671d95325f58edc2d1d5106862
Mohammed <6bouass9@gmail.com>
2024-01-23 09:57:08 -0800

    fixing docscripting errors in Mnadatory tasks

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
-"""Define a Class named Square"""
+"""Square Module"""
 
 
 class Square:
+    """Define a class named Square"""
+
     def __init__(self, size):
         """Constructor:
 
-            Args:
-                size: length of a side of the square
+        Args:
+            size: length of a side of the square
         """
         self.__size = size
