39aeefe18b6063485173d3d184ccc058f8ba4120
Mohammed <6bouass9@gmail.com>
2024-01-23 10:04:45 -0800

    fixing docscripting errors in Mnadatory tasks

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -4,27 +4,21 @@
 
 class Square:
     """Define a Class named Square"""
-    def area(self):
-        """Area of square
 
-        Return:
-            the size squared.
-        """
+    def area(self):
+    """Area of square
+
+    Return:
+        the size squared.
+    """
         return self.__size ** 2
 
     def __init__(self, size=0):
-        """Constructor:
+    """Constructor:
 
-            Args:
-                size: len of a side of the square.
-            Raise:
-                TypeError: if size is not int.
-                ValueError: if size if less than 0.
-            """
-        if not isinstance(size, int):
-            raise TypeError('size must be an integer')
-        if size < 0:
-            raise ValueError('size must be >= 0')
+        Args:
+            len of a side of the square.
+    """
         self.size = size
 
         @property
