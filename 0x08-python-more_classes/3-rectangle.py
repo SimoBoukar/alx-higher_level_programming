e47df8eec1ceb613d0abe8b77b5313d9db6c0dba
Mohammed <6bouass9@gmail.com>
2024-01-29 09:04:24 -0800

    Mandatory tasks

---
+++ 0x08-python-more_classes/3-rectangle.py
@@ -0,0 +1,56 @@
+#!/usr/bin/python3
+"""class Rectangle that defines a rectangle"""
+
+
+class Rectangle:
+    """class Rectangle that defines a rectangle"""
+    def area(self):
+        """return area of a rectangle"""
+        return self.__width * self.__height
+
+    def perimeter(self):
+        """return perimeter of a rectangle"""
+        if self.__width == 0 or self.__height == 0:
+            return 0
+        return ((self.__width * 2) + (self.__height * 2))
+
+    def __init__(self, width=0, height=0):
+        """initialize rectangle"""
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """getter description"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """setter description"""
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """getter description"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """setter description"""
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def __str__(self):
+        """returns printable string"""
+        string = ""
+        if self.__width != 0 and self.__height != 0:
+            string += "\n".join("#" * self.__width
+                    for j in range(self.__height))
+            return string
