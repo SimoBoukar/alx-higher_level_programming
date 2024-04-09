a689aa5daaf9f84aadaef736e9d575580f594117
Mohammed <6bouass9@gmail.com>
2024-04-09 02:41:42 +0000

    fix styles of all tasks

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,15 +1,11 @@
 #!/usr/bin/node
-if (isNaN(process.argv[2])) 
-{
-    console.log('Missing number of occurrences');
-} 
-else 
-{
-    const num = Number(process.argv[2]);
-    let i = 0;
-    while (i < num) 
-      {
-        console.log('C is fun');
-        i++;
-      }
+if (isNaN(process.argv[2])) {
+  console.log('Missing number of occurrences');
+} else {
+  const num = Number(process.argv[2]);
+  let i = 0;
+  while (i < num) {
+    console.log('C is fun');
+    i++;
+  }
 }
--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,15 +1,11 @@
 #!/usr/bin/node
-if (isNaN(process.argv[2])) 
-{
-    console.log('Missing size');
-} 
-else 
-{
-    const num = Number(process.argv[2]);
-    let i = 0;
-    while (i < num) 
-      {
-        console.log('X'.repeat(num));
-        i++;
-      }
+if (isNaN(process.argv[2])) {
+  console.log('Missing size');
+} else {
+  const num = Number(process.argv[2]);
+  let i = 0;
+  while (i < num) {
+    console.log('X'.repeat(num));
+    i++;
+  }
 }
