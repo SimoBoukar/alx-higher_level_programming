0d0a0681f57230a0abbe3c7ddfbb297cb584d6e4
Mohammed <6bouass9@gmail.com>
2024-04-25 23:15:05 +0100

    push advanced tasks v2

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Bash script that sends a JSON POST request to a URL passed as the first argument, and displays the body of the response
-curl -sX POST "$1" -H "Content-Type: application/json" -d @"$2"
+curl -sX POST -d  "@$2" -H "Content-Type: application/json" "$1"
--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Catch me
-curl "0.0.0.0:5000/catch_me" -sLX PUT -H "Origin: HolbertonSchool" -d "user_id=98"
+curl -sX PUT -L -d "user_id=98" -H "Origin:HolbertonSchool" "0.0.0.0:5000/catch_me"
