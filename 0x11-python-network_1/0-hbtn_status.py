12be5a3f45aca066bb1468d2cf0138929c67fb9b
Mohammed <6bouass9@gmail.com>
2024-04-27 00:10:30 +0100

    push mandatory tasks

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+"""urllib usage"""
+import urllib.request
+import urllib.parse
+
+
+if __name__ == "__main__":
+    with urllib.request.urlopen('https://intranet.hbtn.io/status') as resp:
+        html = resp.read()
+        print('Body response:')
+        print('\t- type:', type(html))
+        print('\t- content:', html)
+        print('\t- utf8 content:', html.decode('UTF-8'))
