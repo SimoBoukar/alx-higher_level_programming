28535e46b111bbc305066182d0d28eb6d4dffe99
Mohammed <6bouass9@gmail.com>
2024-04-27 00:21:42 +0100

    push mandatory task 10

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -9,6 +9,11 @@ import requests
 
 
 if __name__ == "__main__":
-    auth = HTTPBasicAuth(sys.argv[1], sys.argv[2])
-    r = requests.get("https://api.github.com/user", auth=auth)
-    print(r.json().get("id"))
+    r = requests.get(
+            'https://api.github.com/user',
+            auth=(argv[1], argv[2]))
+    if r.status_code == 200:
+        j = r.json()
+        print(j.get('id'))
+    else:
+        print('None')
